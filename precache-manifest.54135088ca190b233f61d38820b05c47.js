85e53c9c6cf3c25b9cad86f0e705804fa5a4db58
vicspider2001 <anogwihvictor@gmail.com>
2022-07-16 21:41:06 +0100

    Chizaram

---
+++ precache-manifest.54135088ca190b233f61d38820b05c47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29ca0536a6da81abaccaec3b75026a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d89fae3fceaf688cc77",
+    "url": "/static/css/main.fc8c12e5.chunk.css"
+  },
+  {
+    "revision": "2d252748673e3423b0cc",
+    "url": "/static/js/2.bbf449c5.chunk.js"
+  },
+  {
+    "revision": "0d89fae3fceaf688cc77",
+    "url": "/static/js/main.9cfdeac6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
